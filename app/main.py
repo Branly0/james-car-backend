0da470983e22b734d01e0ba90732c12001c40c10
Branly0 <nkasibranly01@gmail.com>
2026-01-19 16:47:23 +0100

    Refactor main.py by removing migration code
    Removed migration logic and unnecessary imports.

--- app/main.py
+++ app/main.py
@@ -5,23 +5,10 @@ from app.db.base import Base
 from app.db.session import engine
 
 from fastapi.staticfiles import StaticFiles
-
-from alembic.config import Config
-from alembic import command
-
-def run_migrations():
-    # Load the alembic configuration from your alembic.ini file
-    alembic_cfg = Config("alembic.ini")
-    
-    # Run the 'upgrade head' command
-    command.upgrade(alembic_cfg, "head")
-
-if __name__ == "__main__":
-    run_migrations()
-    # ... your app startup code here ...
-
-
 from fastapi.middleware.cors import CORSMiddleware
+
+
+Base.metadata.create_all(engine)
 
 
 app = FastAPI(title="MR JAMES AFFORDABLE USED CARS API")
