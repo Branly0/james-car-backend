417b03419648848ca52d32df487143d9f5e79f02
Branly0 <nkasibranly01@gmail.com>
2026-02-04 23:25:54 +0100

    Remove unused origin from CORS middleware

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ app.include_router(favorites.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://www.jamescarafortable.com","https://james-admin.vercel.app","https://james-car-frontend-h7ui.vercel.app/catalogue"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://www.jamescarafortable.com","https://james-admin.vercel.app","https://james-car-frontend-h7ui.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
